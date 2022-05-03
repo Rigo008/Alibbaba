c1a988079760e3622ce38d75d9444989aa23ae54
rigoberto <rcastellanos@scl.edu.gt>
2022-05-02 20:58:08 -0600

    trabajo concluido

---
+++ js/orders_has_products.js
@@ -0,0 +1,12 @@
+//Base de datos con el id de las compras existentes y el id de los ventas registrados.
+
+
+let OrdersPaid = [
+    [256324,	1000],
+    [135632,	1001],
+    [684323,	1002],
+    [318795,	1003],
+    [356547,	1004],
+    [653214,	1005],
+    [135468,	1006]
+]
---
+++ js/user_has_creditcard.js
@@ -0,0 +1,11 @@
+//Base de datos sobre el id del cliente y el número de tarjeta de crédito que se asigno.
+
+let UserCreditcard = [
+    [100,	2.56324]
+    [101,	1.35632]
+    [102,	6.84323]
+    [103,	3.18795]
+    [104,	3.56547]
+    [105,	6.53214]
+    [106,	1.35468]
+]
