8a17577357e858207c9ea4a9811778e1deff0942
rigoberto <rcastellanos@scl.edu.gt>
2022-05-17 20:49:09 -0600

    login terminado

---
+++ js/administrador.js
@@ -0,0 +1,8 @@
+import {section_administrador, section_login} from './login.js';
+
+export function adminstrador(){
+    alert('Soy Administrador');
+    section_administrador.classList.remove("block");
+    section_login.classList.remove("activar-flex");
+}
+
